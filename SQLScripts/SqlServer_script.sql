2d4290618977d49e70d4133e9cc57e0a14ea6ad2
Alexandre Patrocinio <patrocinio.alexandre@hotmail.com>
2024-05-01 13:52:29 -0300

    add SQL scripts files

---
+++ SQLScripts/SqlServer_script.sql
@@ -0,0 +1,40 @@
+	IF OBJECT_ID(N'dbo.Customers', N'U') IS NULL
+	begin
+                        CREATE TABLE Customers (
+                            ID uniqueidentifier NOT NULL,
+                            Alias nvarchar(32) NOT NULL,
+                            Name varchar(100) NOT NULL,
+                            Birthdate datetime NOT NULL,
+                            Stack nvarchar(4000),
+                            Search AS LOWER(COALESCE(Alias,'') + ' ' + COALESCE(Name,'') + ' ' + COALESCE(Stack,'')) PERSISTED,
+                            PRIMARY KEY (ID)
+                        );
+
+                        IF OBJECT_ID(N'dbo.idx_search', N'IX') IS NULL
+                            CREATE INDEX idx_search ON Customers (Search) USING GIST;
+
+                        IF OBJECT_ID(N'dbo.idx_alias', N'IX') IS NULL
+                            CREATE INDEX idx_alias ON Customers (Alias);
+	end
+
+
+	IF OBJECT_ID(N'dbo.Companies', N'U') IS NULL
+	begin
+	      CREATE TABLE Companies(
+                 	          ID uniqueidentifier NOT NULL,                        
+                           TradeName nvarchar(100) NOT NULL,
+	          BusinessSector nvarchar(32) unique NOT NULL,
+	          NumberEmployees int,
+                           FoundingDate datetime NOT NULL,
+                           ServiceProductCatalog nvarchar(4000),
+                           Search AS LOWER(
+                            COALESCE(TradeName,'') + ' ' + COALESCE(BusinessSector,'') + ' ' + COALESCE(ServiceProductCatalog,''))
+                           ) PERSISTED,
+                           PRIMARY KEY (ID)
+                        );
+
+                        IF OBJECT_ID(N'dbo.idx_search', N'IX') IS NULL
+                            CREATE INDEX idx_search ON Companies (Search) USING GIST;                   
+
+                        IF OBJECT_ID(N'dbo.idx_tradename', N'IX') IS NULL
+                            CREATE INDEX idx_tradename ON Companies (TradeName);
---
+++ SQLScripts/pg_script.sql
@@ -0,0 +1,35 @@
+	CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+	CREATE TABLE IF NOT EXISTS Persons(
+                        ID uuid NOT NULL,
+                        Alias varchar(32) unique NOT NULL,
+                        Name varchar(100) NOT NULL,
+                        Birthdate timestamp NOT NULL,
+                        Stack varchar(4000),
+                        Search varchar(1160) generated always as (
+                            LOWER(COALESCE(Alias,'') || ' ' || COALESCE(Name,'') || ' ' || COALESCE(Stack,''))
+                        ) stored,
+                        PRIMARY KEY (ID)
+                    );
+                    
+                    CREATE INDEX IF NOT EXISTS idx_search on Persons USING gist (Search gist_trgm_ops);
+
+                    CREATE INDEX IF NOT EXISTS idx_alias on Persons USING btree (Alias);
+
+
+	CREATE TABLE IF NOT EXISTS Companies(
+                        ID uuid NOT NULL,                        
+                        TradeName varchar(100) NOT NULL,
+	      BusinessSector varchar(32) unique NOT NULL,
+	      NumberEmployees int,
+                        FoundingDate timestamp NOT NULL,
+                        ServiceProductCatalog varchar(4000),
+                        Search varchar(1160) generated always as (
+                            LOWER(COALESCE(TradeName,'') || ' ' || COALESCE(BusinessSector,'') || ' ' || COALESCE(ServiceProductCatalog,''))
+                        ) stored,
+                        PRIMARY KEY (ID)
+                    );
+                    
+                    CREATE INDEX IF NOT EXISTS idx_search on Companies USING gist (Search gist_trgm_ops);
+
+                    CREATE INDEX IF NOT EXISTS idx_TradeName on Companies USING btree (TradeName);
