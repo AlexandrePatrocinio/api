e2110a2a8b7e7c18072963bea3697e8547cbbdf8
Alexandre Patrocinio <patrocinio.alexandre@hotmail.com>
2024-06-23 15:57:55 -0300

    Add modified api files

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@ using api.Models;
 using api.Services;
 using AutoCRUD.Extensions;
 
+var env = Environment.GetEnvironmentVariables();
+
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+var configuration = builder.Configuration.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Configuration"))
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env["EnvironmentName"]}.json", optional: true, reloadOnChange: true)
+        .Build();
 
 var app = builder
     .AddServiceAutoCRUDValidation<Person, ServicePersonValidation<Person>>()
