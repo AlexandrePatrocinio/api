8b03c97dc320b0b90e0a5a75d6bc8bb6a484641d
Alexandre Patrocinio <ptrn.alexandre@gmail.com>
2024-07-30 18:35:36 -0300

    Add UserSecrets + SqlClient repositorys

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "Configuration"))
         .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
         .AddJsonFile($"appsettings.{env["EnvironmentName"]}.json", optional: true, reloadOnChange: true)
+        .AddUserSecrets<Program>()
         .Build();
 
 var app = builder
     .AddServiceAutoCRUDValidation<Person, ServicePersonValidation<Person>>()
-    .AddNpgSqlRepository<Person>("Persons", "ID", configuration.GetConnectionString("api") ?? string.Empty, "Search")
-    .AddServiceAutoCRUDValidation<Companie, ServiceCompanieValidation<Companie>>()
-    .AddNpgSqlRepository<Companie>("Companies", "ID", configuration.GetConnectionString("api") ?? string.Empty, "Search")    
+    .AddSqlClientRepository<Person>("Persons", "ID", configuration.GetConnectionString("api") ?? string.Empty, "Search")
+    .AddSqlClientRepository<Companie>("Companies", "ID", configuration.GetConnectionString("api") ?? string.Empty, "Search")    
     .Build();
 
 app
